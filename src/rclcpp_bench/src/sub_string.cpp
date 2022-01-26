f8bcbe31b9cecde5fe3484d3a6e82d2f0946f0bc
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-01-26 18:49:03 +0900

    auto format on VS Code

--- src/rclcpp_bench/src/sub_string.cpp
+++ src/rclcpp_bench/src/sub_string.cpp
@@ -6,23 +6,23 @@ using std::placeholders::_1;
 
 class MinimalSubscriber : public rclcpp::Node
 {
-  public:
-    MinimalSubscriber()
-    : Node("sub_node")
-    {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "testtopic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-    }
+public:
+  MinimalSubscriber()
+      : Node("sub_node")
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+        "testtopic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+  }
 
-  private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-    }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+private:
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
