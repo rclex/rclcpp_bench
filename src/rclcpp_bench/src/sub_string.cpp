c871d1097b17c536b156280df5e0a65d0f2a68ae
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-01-26 19:58:21 +0900

    stop sub_node when subscribed "end"

--- src/rclcpp_bench/src/sub_string.cpp
+++ src/rclcpp_bench/src/sub_string.cpp
@@ -4,6 +4,8 @@
 #include "std_msgs/msg/string.hpp"
 using std::placeholders::_1;
 
+using namespace std::chrono_literals;
+
 class MinimalSubscriber : public rclcpp::Node
 {
 public:
@@ -18,6 +20,13 @@ private:
   void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
     RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+
+    std::string msg_string = msg->data.c_str();
+    if (msg_string == "end")
+    {
+      rclcpp::sleep_for(1000ms);
+      rclcpp::shutdown();
+    }
   }
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
